131d6f598dd66bf1d020f4909f862c6bbdf7dfe0
Shea <sriordan3@ycp.edu>
2020-03-15 19:32:42 -0400

    Advisors class inherits from user

--- CS320_YCPCommencement/src/edu/ycp/cs320/lab02a_smelendez/model/Advisors.java
+++ CS320_YCPCommencement/src/edu/ycp/cs320/lab02a_smelendez/model/Advisors.java
@@ -7,32 +7,13 @@ package edu.ycp.cs320.lab02a_smelendez.model;
 //    a call to model.getMin()
 // when the JSP specifies if(game.done), that gets converted to
 //    a call to model.isDone()
-public class Advisors {
-	private String username;
-	private String password;
+public class Advisors inherits User{
 	
 	public Advisors() 
 	{
 		
 	}
 	
-	public void setUsername(String userName) 
-	{
-		this.username=userName;
-	}
-	
-	public String getUsername() 
-	{
-		return username;
-	}
-	
-	public void setPassword(String passWord) {
-		this.password = passWord;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
+
 
 }
