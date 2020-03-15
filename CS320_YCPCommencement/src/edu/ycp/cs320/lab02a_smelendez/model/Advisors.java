0272e709555fac6d08207f0d815be1cd56526773
Shea <sriordan3@ycp.edu>
2020-03-15 19:37:30 -0400

    Advisor constructor fix

--- CS320_YCPCommencement/src/edu/ycp/cs320/lab02a_smelendez/model/Advisors.java
+++ CS320_YCPCommencement/src/edu/ycp/cs320/lab02a_smelendez/model/Advisors.java
@@ -1,17 +1,12 @@
 package edu.ycp.cs320.lab02a_smelendez.model;
 
-// model class for GuessingGame
-// only the controller should be allowed to call the set methods
-// the JSP will call the "get" and "is" methods implicitly
-// when the JSP specifies game.min, that gets converted to
-//    a call to model.getMin()
-// when the JSP specifies if(game.done), that gets converted to
-//    a call to model.isDone()
+// model class for Advisor
+//type of user, inherits username and password
 public class Advisors inherits User{
 	
-	public Advisors() 
+	public Advisors(String username, String password) 
 	{
-		
+		super(username, password);
 	}
 	
 
