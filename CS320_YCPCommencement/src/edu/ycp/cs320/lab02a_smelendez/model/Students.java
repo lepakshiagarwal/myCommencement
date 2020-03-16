f284ed63892d0ab1e4893693d37bfaf2221debc9
Acanzano6613 <60419148+Acanzano6613@users.noreply.github.com>
2020-03-15 20:18:04 -0400

    Update Students.java

--- CS320_YCPCommencement/src/edu/ycp/cs320/lab02a_smelendez/model/Students.java
+++ CS320_YCPCommencement/src/edu/ycp/cs320/lab02a_smelendez/model/Students.java
@@ -1,21 +1,49 @@
 package edu.ycp.cs320.lab02a_smelendez.model;
 
-public class Students extends User {
-	private int qrCode;
+// model class for GuessingGame
+// only the controller should be allowed to call the set methods
+// the JSP will call the "get" and "is" methods implicitly
+// when the JSP specifies game.min, that gets converted to
+//    a call to model.getMin()
+// when the JSP specifies if(game.done), that gets converted to
+//    a call to model.isDone()
+public class Students extends User{
+	private String username;
+	private String password;
+	private String test1 = "Stu Dent";
+	private String test2 = "wordpass";
 	
-	//inheriting username and password from user class
-	public Students(username,password) {
-		super(username,password);
+	public Students() {
 	}
 	
-	//setting qrCode
-	public void setQrCode(int qrcode)) {
-		this.qrCode = qrCode;
+	public void setUsername(String userName) 
+	{
+		this.username=userName;
 	}
 	
-	//get method for qrCode
-	public Double getQrCode() {
-		return qrCode;
+	public String getUsername() 
+	{
+		return username;
 	}
 	
+	public void setPassword(String passWord) {
+		this.password = passWord;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public boolean isValid() {
+		if(test1 == getUsername() ) {
+			if(test2 == getPassword() ) {
+				return true;
+			}else {
+				return false;
+			}
+		}else {
+			return false;
+		}
+		
+	}
 }
