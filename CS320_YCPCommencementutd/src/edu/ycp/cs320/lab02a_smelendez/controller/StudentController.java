c7f4f41556d65e008f8f96cb21dbf1bb991e4ec8
Acanzano6613 <60419148+Acanzano6613@users.noreply.github.com>
2020-03-15 23:34:49 -0400

    Up to date project

---
+++ CS320_YCPCommencementutd/src/edu/ycp/cs320/lab02a_smelendez/controller/StudentController.java
@@ -0,0 +1,19 @@
+package edu.ycp.cs320.lab02a_smelendez.controller;
+
+
+import edu.ycp.cs320.lab02a_smelendez.model.Students;
+
+public class StudentController {
+	
+	private Students model;
+	
+	public void setModel(Students model) {
+		this.model = model;
+	}
+	
+	
+
+
+	
+}
+
---
+++ CS320_YCPCommencementutd/src/edu/ycp/cs320/lab02a_smelendez/model/Advisors.java
@@ -0,0 +1,31 @@
+package edu.ycp.cs320.lab02a_smelendez.model;
+
+public class Advisors{
+	
+	private String username;
+	private String password;
+	
+	public Advisors() {
+	}
+	
+	public void setUsername(String userName) 
+	{
+		this.username=userName;
+	}
+	
+	public String getUsername() 
+	{
+		return username;
+	}
+	
+	public void setPassword(String passWord) {
+		this.password = passWord;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	
+	
+	}
