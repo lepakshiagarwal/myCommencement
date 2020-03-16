36491f449ffd97e1b1be35f162a466ec954d6384
Acanzano6613 <60419148+Acanzano6613@users.noreply.github.com>
2020-03-15 20:18:34 -0400

    Update StudentController.java

--- CS320_YCPCommencement/src/edu/ycp/cs320/lab02a_smelendez/controller/StudentController.java
+++ CS320_YCPCommencement/src/edu/ycp/cs320/lab02a_smelendez/controller/StudentController.java
@@ -7,26 +7,16 @@ public class StudentController {
 	
 	private Students model;
 	
-	//setting the model
 	public void setModel(Students model) {
 		this.model = model;
 	}
 	
-	
-	public void choose() {
-		return view;
+	public boolean Validate() {
+		return model.isValid();
+		
 	}
-	
-	public void populate() {
-		return template;
-	}
-	
-	public void edit() {
-		return content;
-	}
-	
-	public void upload() {
-		return view;
-	}
+
+
 	
 }
+
